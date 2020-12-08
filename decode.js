c4042acac375dd0b3a263705b937d10de28e8c04
eliskol <ge0447el0828@pusd.us>
2020-12-08 18:54:51 +0000

    commented my code :}

--- decode.js
+++ decode.js
@@ -1,47 +1,54 @@
 function decode() {
-  var shift = parseInt(document.getElementById('shift').value);
+  var shift = parseInt(document.getElementById('shift').value); 
+
   var text = document.getElementById("text").value;
-  let x = [];
+
+  let x = []; //empty array to hold output
 
 
-if(shift < 26 && shift > -26){
-  for (var i = 0; i < text.length; i++) {
+if(shift < 26 && shift > -26){ 
+  //make sure the shift is between -26 and 26
+    for (var i = 0; i < text.length; i++) {
 
-    if ((text.charCodeAt(i) <= 122 && text.charCodeAt(i) >= 97) || (text.charCodeAt(i) <= 90 && text.charCodeAt(i) >= 65)) { //checks that the input is a letter
+      if ((text.charCodeAt(i) <= 122 && text.charCodeAt(i) >= 97) || (text.charCodeAt(i) <= 90 && text.charCodeAt(i) >= 65)) { //checks that the input is a letter, only shift if the input is a letter
+        
+        x[i] = text.charCodeAt(i) - shift;
+        //shifts
 
-      x[i] = text.charCodeAt(i) - shift;
+        if (x[i] > 122) { //if the shifted charcode is greater than the charcode for z, loop back to the beginning of the alphabet
+          x[i] -= 26;
+        }
 
-      if(x[i] > 122){ //if the shifted charcode is greater than the charcode for z, loop back to the beginning of the alphabet
-        x[i] -= 26;
+        else if ((x[i] < 97 && (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122))) { //if the shifted character isn't a lowercase letter like the input
+          x[i] += 26;
+        }
+
+        else if (x[i] > 90 && (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90)) {
+          //if the shifted character isn't a capital letter, if the shifted charcode is greater than the charcode for Z
+
+          x[i] -= 26;
+        }
+
+        else if (x[i] < 65 && (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90)) {
+          //if the shifted character is not a capital letter, if the shifted charcode is less than the charcode for A
+          x[i] += 26;
+        }
       }
-      
-      else if((x[i] < 97 && (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122))){ //if the shifted character isn't a lowercase letter like the input
-        x[i] += 26;
+
+      else {
+        //if the input isn't a letter, don't shift it
+        x[i] = text.charCodeAt(i);
       }
-      
-      else if((x[i] < 97 && x[i] > 90) && (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90)){
-        x[i] -= 26;
-      }
-      
-      else if(x[i] < 65 && (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90)){
-        x[i] += 26;
-      }
-      else if(x[i]){
-              
-      }
-    } 
-    else{
-      x[i] = text.charCodeAt(i);
+
     }
 
-  }
 
-
-
+  //create a heading for the decoded text
   var dc = document.createElement('p');
   dc.innerText = "Decoded text:";
   document.body.appendChild(dc);
 
+  //push the decoded text to the DOM
   var decstring = String.fromCharCode.apply(null, x);
   var p = document.createElement('p');
   p.innerText = decstring;
@@ -50,6 +57,8 @@ if(shift < 26 && shift > -26){
 }
 
   else{
+    //if the shift is too big or too small, push this error message
+
     var pf = document.createElement('p');
     pf.innerText = "Error: shift must be between -26 and 26."
     document.body.appendChild(pf);
